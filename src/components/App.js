8e51dd2c67ac95f0e643f4d81544fb3d34e18e92
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-03-11 11:30:29 +0100

    added css to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import '../App.css';
+import Header from "./Header/Header";
+import TextBubble from "./TextBubble/TextBubble";
+
 
 function App() {
   return (
-    <div>
-         Dit is mijn eind project "Sociaal Oosterhout"
-    </div>
+    <>
+        <div>
+            Dit is mijn eind project "Sociaal Oosterhout"
+        </div>
+
+        <Header />
+
+        <TextBubble />
+    </>
   );
 }
 
---
+++ src/components/TextBubble/TextBubble.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TextBubble.css";
+
+const TextBubble = () => {
+    return (
+        <div className={"styleText"}>TEXTBUBBLE</div>
+    )
+}
+
+export default TextBubble
