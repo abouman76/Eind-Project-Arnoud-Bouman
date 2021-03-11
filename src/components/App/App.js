67a84ed68af12a86057bb5f9723abb72dcff922e
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-03-11 11:56:11 +0100

    structured components and src folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
-import '../App.css';
-import Header from "./Header/Header";
-import TextBubble from "./TextBubble/TextBubble";
+import './App.css';
+import Header from "../Header/Header";
+import TextBubble from "../TextBubble/TextBubble";
 
 
 function App() {
