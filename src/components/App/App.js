c7cd987204b6a3d446f6e853c5deba1f3a7a64ad
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-03-23 10:18:46 +0100

    imported all pages to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,43 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import HomePage from "../../pages/HomePage";
+import AanbodPage from "../../pages/AanbodPage";
+import VraagPage from "../../pages/VraagPage";
+import ContactPage from "../../pages/ContactPage";
+import LoginPage from "../../pages/LoginPage";
 // import Header from "../HeaderGeneral/Header";
-// import TextBubble from "../TextBubble/TextBubble";
-// import BtnCtaHome from "../Buttons/BtnCtaHome";
-// import HeaderPersonal from "../HeaderPersonal/HeaderPersonal";
-import Contact from "../Contact/Contact"
+// import Contact from "../Contact/Contact"
 // import BtnSendContact from "../Buttons/BtnSendContact";
 
 
 function App() {
   return (
-    <>
-            {/*<Header />*/}
+    <Router>
+        {/*<Header />*/}
+        <Switch>
+            <Route exact path="/">
+                <HomePage />
+            </Route>
+            <Route path="/">
+                <AanbodPage />
+            </Route>
+            <Route path="/">
+                <VraagPage />
+            </Route>
+            <Route path="/">
+                <ContactPage />
+            </Route>
+            <Route path="/">
+                <LoginPage />
+            </Route>
 
-            {/*<HeaderPersonal />*/}
+        </Switch>
 
-            <Contact />
-
-            {/*<TextBubble />*/}
-
-            {/*<BtnCtaHome />*/}
-
-            {/*<BtnSendContact />*/}
-
-    </>
+    </Router>
   );
 };
 
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const LoginPage = () => {
+    return (
+        <div> </div>
+    )
+}
+
+export default LoginPage;
