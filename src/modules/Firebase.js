83fb8203f315003b212cf8a17d335940f871b689
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-04-13 12:48:04 +0200

    added firebase script in modules

--- src/modules/Firebase.js
+++ src/modules/Firebase.js
@@ -1,6 +1,12 @@
 // import React from "react";
 import firebase from "firebase"
 //
-// // const dataBase =
-//
+const dataBase = firebase.initializeApp({
+    apiKey: "AIzaSyDqioo9gvrONY6KZv8u6yfD8GufAUH3hqQ",
+    authDomain: "sociaal-oosterhout.firebaseapp.com",
+    projectId: "sociaal-oosterhout",
+    storageBucket: "sociaal-oosterhout.appspot.com",
+    messagingSenderId: "110680207398",
+    appId: "1:110680207398:web:da370238464723b36c76e7"
+})
 export default dataBase;
