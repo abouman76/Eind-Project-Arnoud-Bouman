cfbb29ddfca797a4016bb14c5ee4702b36a6088e
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-05-03 15:37:06 +0200

    test with new firebase api

--- src/modules/Firebase.js
+++ src/modules/Firebase.js
@@ -2,11 +2,19 @@
 import firebase from "firebase"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "sociaal-oosterhout.firebaseapp.com",
-    projectId: "sociaal-oosterhout",
-    storageBucket: "sociaal-oosterhout.appspot.com",
-    messagingSenderId: process.env.REACT_APP_API_MSG,
-    appId: process.env.REACT_APP_API_ID
+
+    apiKey: "AIzaSyBsIR0XxhYw7xexmlXDglU0xstGNZJt1-E",
+    authDomain: "eind-opdracht-novi.firebaseapp.com",
+    projectId: "eind-opdracht-novi",
+    storageBucket: "eind-opdracht-novi.appspot.com",
+    messagingSenderId: "790917037324",
+    appId: "1:790917037324:web:49e934a308aa6117a89d29"
+
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: "sociaal-oosterhout.firebaseapp.com",
+    // projectId: "sociaal-oosterhout",
+    // storageBucket: "sociaal-oosterhout.appspot.com",
+    // messagingSenderId: process.env.REACT_APP_API_MSG,
+    // appId: process.env.REACT_APP_API_ID
 });
 export default app;
