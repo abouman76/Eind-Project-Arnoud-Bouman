ae4e7b8f5225224a717946abf72b8a1a2e1f7330
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-05-03 19:52:19 +0200

    env process

--- src/modules/Firebase.js
+++ src/modules/Firebase.js
@@ -1,15 +1,16 @@
-// import React from "react";
 import firebase from "firebase"
 
 const app = firebase.initializeApp({
 
-    apiKey: "AIzaSyBsIR0XxhYw7xexmlXDglU0xstGNZJt1-E",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "eind-opdracht-novi.firebaseapp.com",
     projectId: "eind-opdracht-novi",
     storageBucket: "eind-opdracht-novi.appspot.com",
-    messagingSenderId: "790917037324",
-    appId: "1:790917037324:web:49e934a308aa6117a89d29"
+    messagingSenderId: process.env.REACT_APP_API_MSG,
+    appId: process.env.REACT_APP_API_ID
 
+
+    // OUDE API
     // apiKey: process.env.REACT_APP_API_KEY,
     // authDomain: "sociaal-oosterhout.firebaseapp.com",
     // projectId: "sociaal-oosterhout",
